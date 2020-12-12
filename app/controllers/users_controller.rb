7966a949f5ffd80b65b3de239cd12477c49c1000
かずき <skaz0110@yahoo.co.jp>
2020-12-12 23:53:08 +0900

    resourcesを使わないCRUDアプリを作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,37 @@
+class UsersController < ApplicationController
+    def index
+        @users = User.all
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        User.create(user_params)
+        redirect_to users_path
+    end
+
+    def edit
+        @user = User.find(params[:id])
+        
+    end
+
+    def update
+        user = User.find(params[:id])
+        user.update(user_params)
+        redirect_to users_path
+    end
+
+    def destroy
+        user = User.find(params[:id])
+        user.destroy
+        redirect_to users_path
+    end
+    
+
+    private
+    def user_params
+        params.require(:user).permit(:name, :age)
+    end
+end
